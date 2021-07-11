f3ff00da98706f53fe02f5e28af785fad8347e16
Sean Glass <scglass13@gmail.com>
2021-07-11 12:45:59 -0500

    Added Promises Assignment Files

---
+++ Promises_Assignment_1/src/main.js
@@ -0,0 +1,7 @@
+const { welcome, goodbye, tell } = require("../utils/fortune-teller");
+
+function getFortune(question) {}
+
+function fullSession(question) {}
+
+module.exports = { getFortune, fullSession };
